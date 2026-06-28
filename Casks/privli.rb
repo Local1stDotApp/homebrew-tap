40daea5e136e8f0d5ee4e2773f535b03923d097c
privli-ci <ci@local1st.app>
2026-06-28 16:46:31 +0800

    privli 2.6.0

--- Casks/privli.rb
+++ Casks/privli.rb
@@ -1,6 +1,6 @@
 cask "privli" do
-  version "2.5.0"
-  sha256 "f3057edb3c9fa8e1e30c72e021eefc31770e71c3da5539665707f8e549b446e2"
+  version "2.6.0"
+  sha256 "f7432600054274782ec0cbb6b2356962772b876bab13b3fd522fa8b7e53155d9"
 
   url "https://github.com/Local1stDotApp/paste_share_open_source/releases/download/latest/Privli-#{version}-macos-universal.dmg",
       verified: "github.com/Local1stDotApp/paste_share_open_source/"
